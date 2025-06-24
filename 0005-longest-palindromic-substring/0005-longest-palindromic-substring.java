a43cb9dd683dee684e24e9603682228a9b723435
Alessandro Pizzetti <a.pspizzetti@gmail.com>
2025-06-24 22:13:59 +0200

    Time: 15 ms (86.1%), Space: 42.1 MB (88.25%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -6,13 +6,13 @@ class Solution {
         int end = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            int len1 = expandAroundCenter(s, i, i);       // palíndromo de tamanho ímpar
-            int len2 = expandAroundCenter(s, i, i + 1);   // palíndromo de tamanho par
+            int len1 = expandAroundCenter(s, i, i);       
+            int len2 = expandAroundCenter(s, i, i + 1);  
             int len = Math.max(len1, len2);
 
             if (len > (end - start)) {
-                start = i - (len - 1) / 2; // novo início
-                end = i + len / 2;         // novo fim
+                start = i - (len - 1) / 2;
+                end = i + len / 2;         
             }
         }
 
@@ -20,12 +20,10 @@ class Solution {
     }
 
     private int expandAroundCenter(String s, int left, int right) {
-        // Expande para os lados enquanto for palíndromo
         while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             left--;
             right++;
         }
-        // Comprimento do palíndromo
         return right - left - 1;
     }
 }
