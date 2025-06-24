f58ed7f34983319aa5c7936d5826070f3495fbac
Alessandro Pizzetti <a.pspizzetti@gmail.com>
2025-06-24 22:29:22 +0200

    Time: 7 ms (97.77%), Space: 42.4 MB (65.46%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,26 +1,27 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if (s.length() <= 1) return s;
+        if (s == null || s.length() < 1) return "";
 
         int start = 0;
-        int end = 0;
+        int maxLen = 0;
+        char[] chars = s.toCharArray();
 
         for (int i = 0; i < s.length(); i++) {
-            int len1 = expandAroundCenter(s, i, i);       
-            int len2 = expandAroundCenter(s, i, i + 1);  
+            int len1 = expandAroundCenter(chars, i, i);
+            int len2 = expandAroundCenter(chars, i, i + 1);
             int len = Math.max(len1, len2);
 
-            if (len > (end - start)) {
+            if (len > maxLen) {
+                maxLen = len;
                 start = i - (len - 1) / 2;
-                end = i + len / 2;         
             }
         }
 
-        return s.substring(start, end + 1);
+        return s.substring(start, start + maxLen);
     }
 
-    private int expandAroundCenter(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+    private int expandAroundCenter(char[] chars, int left, int right) {
+        while (left >= 0 && right < chars.length && chars[left] == chars[right]) {
             left--;
             right++;
         }
