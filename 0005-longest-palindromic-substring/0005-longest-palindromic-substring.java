0ab4a34ea6834ae35856250497a2c9967ab0218d
Alessandro Pizzetti <a.pspizzetti@gmail.com>
2025-06-24 22:30:24 +0200

    Time: 6 ms (99.2%), Space: 43.4 MB (55.18%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,30 +1,48 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if (s == null || s.length() < 1) return "";
+        if (s == null || s.length() == 0) return "";
 
-        int start = 0;
-        int maxLen = 0;
-        char[] chars = s.toCharArray();
-
+        // Transformar a string: exemplo "abba" -> "^#a#b#b#a#$"
+        StringBuilder t = new StringBuilder("^");
         for (int i = 0; i < s.length(); i++) {
-            int len1 = expandAroundCenter(chars, i, i);
-            int len2 = expandAroundCenter(chars, i, i + 1);
-            int len = Math.max(len1, len2);
+            t.append("#").append(s.charAt(i));
+        }
+        t.append("#$");
 
-            if (len > maxLen) {
-                maxLen = len;
-                start = i - (len - 1) / 2;
+        char[] chars = t.toString().toCharArray();
+        int[] p = new int[chars.length];
+        int center = 0, right = 0;
+
+        for (int i = 1; i < chars.length - 1; i++) {
+            int mirror = 2 * center - i;
+
+            if (i < right) {
+                p[i] = Math.min(right - i, p[mirror]);
+            }
+
+            // Expandir ao redor do centro
+            while (chars[i + (1 + p[i])] == chars[i - (1 + p[i])]) {
+                p[i]++;
+            }
+
+            // Atualizar centro e limite direito se o palíndromo atual se expandiu além do limite atual
+            if (i + p[i] > right) {
+                center = i;
+                right = i + p[i];
             }
         }
 
-        return s.substring(start, start + maxLen);
-    }
-
-    private int expandAroundCenter(char[] chars, int left, int right) {
-        while (left >= 0 && right < chars.length && chars[left] == chars[right]) {
-            left--;
-            right++;
+        // Encontrar o maior palíndromo
+        int maxLen = 0;
+        int centerIndex = 0;
+        for (int i = 1; i < p.length - 1; i++) {
+            if (p[i] > maxLen) {
+                maxLen = p[i];
+                centerIndex = i;
+            }
         }
-        return right - left - 1;
+
+        int start = (centerIndex - maxLen) / 2;
+        return s.substring(start, start + maxLen);
     }
 }
