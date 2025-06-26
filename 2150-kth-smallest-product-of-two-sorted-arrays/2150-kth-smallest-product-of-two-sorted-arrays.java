22f6933ae0c9f600b718d43800bb195c058b81bd
Alessandro Pizzetti <a.pspizzetti@gmail.com>
2025-06-26 03:20:59 +0200

    Time: 61 ms (100%), Space: 55.7 MB (28.35%) - LeetHub

--- 2150-kth-smallest-product-of-two-sorted-arrays/2150-kth-smallest-product-of-two-sorted-arrays.java
+++ 2150-kth-smallest-product-of-two-sorted-arrays/2150-kth-smallest-product-of-two-sorted-arrays.java
@@ -1,56 +1,72 @@
 class Solution {
     public long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
-        long low = -100_000L * 100_000L;
-        long high = 100_000L * 100_000L;
+        int pivot1 = findFirstZero(nums1);
+        int pivot2 = findFirstZero(nums2);
+        int n1 = nums1.length, n2 = nums2.length;
+        int[] pos1 = Arrays.copyOfRange(nums1, pivot1, n1);
+        int[] pos2 = Arrays.copyOfRange(nums2, pivot2, n2);
+        int[] neg1 = negativeCopy(nums1, pivot1);
+        int[] neg2 = negativeCopy(nums2, pivot2);
 
-        while (low < high) {
-            long mid = low + (high - low) / 2;
 
-            if (countLessEqual(nums1, nums2, mid) < k) {
-                low = mid + 1;
+        int negCount = neg1.length * pos2.length + pos1.length * neg2.length;
+        long s = 1l;
+        if(k > negCount) {
+            k -= negCount;
+            s = 1l;
+        } else {
+            int[] temp = neg2;
+            neg2 = pos2;
+            pos2 = temp;
+            s = -1l;
+            k = negCount - k +1;
+        }
+
+        long start = 0, end = (long)Math.pow(10, 10);
+
+        while(start < end) {
+            long mid = start +(end-start)/2;
+            if((count(neg1, neg2, mid) + count(pos1, pos2, mid) )>= k) {
+                end = mid;
             } else {
-                high = mid;
+                start = mid+1;
             }
         }
 
-        return low;
+        return start * s;
     }
 
-    // Conta quantos produtos nums1[i] * nums2[j] <= x
-    private long countLessEqual(int[] nums1, int[] nums2, long x) {
-        long count = 0;
-
-        for (int a : nums1) {
-            if (a > 0) {
-                // busca quantos b em nums2 com a*b <= x
-                int left = 0, right = nums2.length;
-                while (left < right) {
-                    int mid = (left + right) / 2;
-                    if ((long)a * nums2[mid] <= x) {
-                        left = mid + 1;
-                    } else {
-                        right = mid;
-                    }
-                }
-                count += left;
-            } else if (a < 0) {
-                // busca quantos b em nums2 com a*b <= x
-                int left = 0, right = nums2.length;
-                while (left < right) {
-                    int mid = (left + right) / 2;
-                    if ((long)a * nums2[mid] <= x) {
-                        right = mid;
-                    } else {
-                        left = mid + 1;
-                    }
-                }
-                count += nums2.length - left;
-            } else {
-                // a == 0: todo produto 0 <= x se x >= 0
-                if (x >= 0) count += nums2.length;
+    long count(int[] n1, int[] n2, long max) {
+        long count =0;
+        int j = n2.length-1;
+        for(int i = 0; i < n1.length; i++) {
+            while(j >= 0 && ((long)n1[i] * (long)n2[j]) > max) {
+                j-=1;
             }
+            count += j+1;
         }
 
         return count;
+    }
+
+    private int findFirstZero(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= 0) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    private int[] negativeCopy(int[] nums, int len) {
+        int[] res = new int[len];
+        for (int i = 0; i < len; i++) {
+            res[i] = -1 * nums[len - 1 - i];
+        }
+        return res;
     }
 }
