de33e8d347db2accb07561e1d97c278126828427
Alessandro Pizzetti <a.pspizzetti@gmail.com>
2025-07-25 10:35:44 +0200

    Time: 3 ms (20.94%), Space: 42.6 MB (39.38%) - LeetHub

---
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxSum(int[] nums) {
+        
+        if(nums.length == 1) return nums[0];
+        
+        Set<Integer> unicos = new HashSet<>();
+        
+        for (int numero : nums) {
+            unicos.add(numero);
+        }
+        
+        int maior = Collections.max(unicos);
+        int menor = Collections.min(unicos);
+        int maximo = maior;
+        
+        
+
+        for (int numero : unicos) {
+            if(numero != maior && (maximo + numero) > maximo) {
+                maximo += numero;
+            }    
+        }
+
+        return maximo;
+    }
+}
